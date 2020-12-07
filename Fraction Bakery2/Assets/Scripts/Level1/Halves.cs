05660dab2da9a78f16efce2aa0a4c4f910e3344e
apouba <apouba@uchicago.edu>
2020-12-06 18:01:18 -0600

    got slider value

---
+++ Fraction Bakery2/Assets/Scripts/Level1/Halves.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Halves : MonoBehaviour
+{
+    public Slider cakeFraction;
+    public Button serve;
+    
+    // invoked when a submit button is clicked
+    public void SubmitSliderSetting()
+    {
+        // displays the value of the slider in the console
+        Debug.Log(cakeFraction.value);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = serve.GetComponent<Button>();
+        btn.onClick.AddListener(SubmitSliderSetting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
