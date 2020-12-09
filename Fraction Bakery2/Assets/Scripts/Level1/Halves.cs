f2c7be76990f22383957e68f6dd5076d171f3731
apouba <apouba@uchicago.edu>
2020-12-08 23:26:46 -0600

    5 groups

--- Fraction Bakery2/Assets/Scripts/Level1/Halves.cs
+++ Fraction Bakery2/Assets/Scripts/Level1/Halves.cs
@@ -7,23 +7,68 @@ public class Halves : MonoBehaviour
 {
     public Slider cakeFraction;
     public Button serve;
+    public Button next;
+    public Button wrong;
+
     public float val;
+
     public GameObject correct;
     public GameObject tryAgain;
-    public Button wrong;
-    
+    public GameObject group1;
+    public GameObject group2;
+    public GameObject group3;
+    public GameObject group4;
+    public GameObject group5;
+    public GameObject endscreen;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Button correct_btn = serve.GetComponent<Button>();
-        correct_btn.onClick.AddListener(SubmitSliderSetting);
+        // submit fraction button
+        Button submit_btn = serve.GetComponent<Button>();
+        submit_btn.onClick.AddListener(SubmitSliderSetting);
         val = 0;
 
+        // try again button
         Button wrong_btn = wrong.GetComponent<Button>();
         wrong_btn.onClick.AddListener(WrongAnswer);
         //wrong.OnClick.AddListener(WrongAnswer);
 
+        // next group button
+        Button next_btn = next.GetComponent<Button>();
+        next_btn.onClick.AddListener(NextGroup);
+
+        // if slider is moved
         cakeFraction.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
+       // every group if correct and finished
+
+
+    }
+
+    // next group of people out of 5
+    public void NextGroup()
+    {
+        if(group1.activeSelf)
+        {
+            group1.SetActive(false);
+            group2.SetActive(true);
+            correct.SetActive(false);
+        } else if(group2.activeSelf)
+        {
+            group2.SetActive(false);
+            group3.SetActive(true);
+            correct.SetActive(false);
+        } else if(group3.activeSelf)
+        {
+            group3.SetActive(false);
+            group4.SetActive(true);
+            group5.SetActive(true);
+        } else
+        {
+            endscreen.SetActive(true);
+        }
     }
 
     // when try again button is clicked
