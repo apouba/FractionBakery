7ed163ed1006bee2970a407d79b95468a0fbbf93
apouba <apouba@uchicago.edu>
2020-12-09 19:09:33 -0600

    exit level button on level1

---
+++ Fraction Bakery2/Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitLevel : MonoBehaviour
+{
+
+    // if exit level button is pressed, send back to level map
+    public Button exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button exit_btn = exit.GetComponent<Button>();
+        exit_btn.onClick.AddListener(BackToMap);
+    }
+
+    public void BackToMap()
+    {
+        SceneManager.LoadScene("Levels Map");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
