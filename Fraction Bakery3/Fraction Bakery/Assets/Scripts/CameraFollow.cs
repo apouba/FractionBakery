640c5e4f84dba216c4f984f47c8877f033068c19
apouba <apouba@uchicago.edu>
2020-12-18 15:15:59 -0600

    github

---
+++ Fraction Bakery3/Fraction Bakery/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Target.transform.position.x,
+            Target.transform.position.y,
+            transform.position.z
+            );    
+    }
+}
